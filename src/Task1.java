b5aab082110db8b2674c24977c1ff2a96fd35547
Phail Safiullin <safiullin.fail@yandex.ru>
2023-03-06 00:11:27 +0300

    Add task1

---
+++ src/Task1.java
@@ -0,0 +1,35 @@
+//Вычислить [n-ое треугольного число](сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int numberN = userInputNumb();
+        int factorialOfNumber = findingFactorialOfNumber(numberN);
+        System.out.printf("Факториал числа %d равен:\n%d! = %d.", numberN, numberN, factorialOfNumber);
+    }
+
+    static int userInputNumb() {
+        System.out.print("Введите целое положительно число: ");
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            if (sc.hasNextInt()) {
+                int userNumb = sc.nextInt();
+                if (userNumb >= 0) {
+                    sc.close();
+                    return userNumb;
+                }
+            }
+            System.out.println("Ошибка ввода! Попробуйте еще раз");
+        }
+    }
+
+    static int findingFactorialOfNumber(int numberForFactorial) {
+        if (numberForFactorial <= 1) {
+            return 1;
+        } else {
+            return numberForFactorial * findingFactorialOfNumber(numberForFactorial - 1);
+        }
+    }
+}
