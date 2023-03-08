71a453f92741eab02db850dde0b1ac8b88fda83a
Phail Safiullin <safiullin.fail@yandex.ru>
2023-03-08 21:56:58 +0300

    Решение без массива

--- src/Task2.java
+++ src/Task2.java
@@ -3,15 +3,16 @@ import java.util.ArrayList;
 /*Вывести все простые числа от 1 до 1000*/
 public class Task2 {
     public static void main(String[] args) {
-        int[] arr = new int[1000];
+        /*int[] arr = new int[1000];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i;
-        }
+        }*/
+        int range = 1000;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (searchPrimeNumber(arr[i])) {
-                System.out.print(arr[i] + " ");
+        for (int i = 0; i < range; i++) {
+            if (searchPrimeNumber(i)) {
+                System.out.print(i + " ");
             }
         }
     }
